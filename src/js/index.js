b6aa25dc1afce70da2861460f3906a08095c252c
JorneSaenen <jorne@jsjj.be>
2021-06-13 16:38:34 +0200

    loader added en animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 import '../css/style.scss';
 
+// intro logo
+const topLogo = document.querySelector('.intro__img--one');
+const middleLogo = document.querySelector('.intro__img--two');
+const bottomLogo = document.querySelector('.intro__img--three');
+const loader = document.querySelector('.intro .loader');
+window.onload = () => {
+  loader.style.display = 'none';
+  middleLogo.style.opacity = '1';
+  setTimeout(() => (topLogo.style.opacity = '1'), 1000);
+  setTimeout(() => (bottomLogo.style.opacity = '1'), 2000);
+};
+
 // mobile menu
 const hamburger = document.querySelector('.nav .hamburger');
 const mobileMenu = document.querySelector('.mobile');
@@ -7,6 +19,7 @@ hamburger.onclick = () => {
   hamburger.classList.toggle('active');
   mobileMenu.classList.toggle('mobile--active');
 };
+
 // navigation scroll behavoir
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.onclick = function (e) {
