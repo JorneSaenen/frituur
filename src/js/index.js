071593a6196ad9567fb12abd1c035f3410ded4f8
JorneSaenen <jorne@jsjj.be>
2021-06-13 15:53:11 +0200

    mobile menu logic added

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,19 @@ import '../css/style.scss';
 
 // mobile menu
 const hamburger = document.querySelector('.nav .hamburger');
-hamburger.onclick = () => hamburger.classList.toggle('active');
-
+const mobileMenu = document.querySelector('.mobile');
+hamburger.onclick = () => {
+  hamburger.classList.toggle('active');
+  mobileMenu.classList.toggle('mobile--active');
+};
 // navigation scroll behavoir
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.onclick = function (e) {
     e.preventDefault();
+    if (mobileMenu.classList.contains('mobile--active')) {
+      hamburger.classList.remove('active');
+      mobileMenu.classList.remove('mobile--active');
+    }
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth',
     });
